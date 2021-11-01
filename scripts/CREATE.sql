58eda474ddac78a988e4d4a7c6fa2829475f7b25
Demirkol, Goerkem <gdemirkol315@gmail.com>
2021-11-01 23:02:04 +0100

    create and insert statements added

---
+++ scripts/CREATE.sql
@@ -0,0 +1,181 @@
+CREATE DATABASE AIRBNB;
+USE AIRBNB;
+
+CREATE TABLE account(
+	account_id int PRIMARY KEY,
+	user_name varchar(50),
+	email varchar(50),
+	password_hash varchar(50),
+	first_name varchar(255),
+	last_name varchar(255)
+	);
+CREATE TABLE social_media_type(
+	social_media_type_id int PRIMARY KEY,
+	social_media_platform_name varchar(50)
+	);
+
+CREATE TABLE credit_card_vendor(
+	credit_card_vendor_id int PRIMARY KEY,
+	credit_card_vendor varchar(30)
+	);
+
+CREATE TABLE payment_status(
+	payment_status_id int PRIMARY KEY,
+	payment_status varchar(10)
+	);
+    
+
+CREATE TABLE accomodation_type(
+	accomodation_type_id int PRIMARY KEY,
+	accomodation_type varchar(30)
+	);
+	
+CREATE TABLE address(
+	address_id int PRIMARY KEY,
+	address varchar(255),
+	postal_code varchar(10),
+	city varchar(30),
+	country varchar(30)
+	);
+
+CREATE TABLE currency(
+	currency_id int PRIMARY KEY,
+	iso_code varchar(3),
+	currency_name varchar(20)
+	);
+CREATE TABLE guest(
+	guest_id int PRIMARY KEY,
+	invoice_address_id int,
+	account_id int,
+	profile_description varchar(50),
+	contact_email varchar(50),
+	contact_phone varchar(20),
+    FOREIGN KEY (invoice_address_id) REFERENCES address(address_id),
+    FOREIGN KEY (account_id) REFERENCES account(account_id)
+	);
+CREATE TABLE host(
+	host_id int PRIMARY KEY,
+	invoice_address_id int,
+	account_id int,
+	profile_description varchar(50),
+	contact_email varchar(50),
+	contact_phone varchar(20),
+	bank_account_number varchar(100),
+	iban varchar(100),
+	bic varchar(100),
+    FOREIGN KEY (invoice_address_id) REFERENCES address(address_id),
+    FOREIGN KEY (account_id) REFERENCES account(account_id)
+	);
+
+CREATE TABLE social_media_profile_host(
+	social_media_profile_id int PRIMARY KEY,
+	host_id int,
+	social_media_type_id int,
+	account_url varchar(1000),
+    FOREIGN KEY (host_id) REFERENCES host(host_id),
+    FOREIGN KEY (social_media_type_id) REFERENCES social_media_type(social_media_type_id)
+	);
+
+CREATE TABLE profile_pic_guest(
+	picture_id int PRIMARY KEY,
+	file_location varchar(1000),
+	title varchar(100)
+	);
+
+CREATE TABLE social_media_profile_guest(
+	social_media_profile_id int PRIMARY KEY,
+	guest_id int,
+	social_media_type_id int ,
+	account_url varchar(1000),
+    FOREIGN KEY (guest_id) REFERENCES guest(guest_id),
+    FOREIGN KEY (social_media_type_id) REFERENCES social_media_type(social_media_type_id)
+	);
+
+
+CREATE TABLE accomodation(
+	accomodation_id  int PRIMARY KEY,
+    accomodation_type_id int,
+    address_id int,
+    capacity int,
+    host_id int,
+    description varchar(5000),
+    FOREIGN KEY (address_id) REFERENCES address(address_id),
+    FOREIGN KEY (accomodation_type_id) REFERENCES  accomodation_type(accomodation_type_id),
+    FOREIGN KEY (host_id) REFERENCES host(host_id)
+    );
+CREATE TABLE profile_pic_host(
+	picture_id int PRIMARY KEY,
+	host_id int,
+	file_location varchar(1000),
+	title varchar(100),
+    FOREIGN KEY (host_id) REFERENCES host(host_id)
+	);
+
+
+
+
+
+CREATE TABLE booking_status(
+	booking_status_id int PRIMARY KEY,
+	booking_status varchar(10)
+	);
+
+CREATE TABLE booking(
+	booking_id int PRIMARY KEY,
+	guest_id int,
+	accomodation_id int,
+	booking_from date,
+	booking_until date,
+	booking_status_id int,
+    FOREIGN KEY (guest_id) REFERENCES guest(guest_id),
+    FOREIGN KEY (accomodation_id) REFERENCES accomodation(accomodation_id),
+    FOREIGN KEY (booking_status_id) REFERENCES booking_status(booking_status_id)
+	);
+
+CREATE TABLE review(
+	review_id int PRIMARY KEY,
+	booking_id int,
+	guest_comment varchar(2000),
+	host_comment varchar(2000),
+	guest_rating int,
+	host_rating int,
+    FOREIGN KEY (booking_id) REFERENCES booking(booking_id)
+	);
+
+
+CREATE TABLE payment(
+	payment_id int PRIMARY KEY,
+	booking_id int,
+	total_amount numeric(12,4),
+	payment_date datetime,
+	credit_card_number varchar(50),
+	credit_card_security_number varchar(5),
+	credit_card_validity_date date,
+	credit_card_vendor_id int,
+	commission_amount numeric(12,4),
+	payment_status_id int,
+	currency_id int,
+    FOREIGN KEY (booking_id) REFERENCES booking(booking_id),
+    FOREIGN KEY (credit_card_vendor_id) REFERENCES credit_card_vendor(credit_card_vendor_id),
+    FOREIGN KEY (payment_status_id) REFERENCES payment_status(payment_status_id)
+	);
+
+CREATE TABLE accommodation_pic(
+	picture_id int PRIMARY KEY,
+	accomodation_id int,
+	file_location varchar(1000),
+	title varchar(100),
+    FOREIGN KEY (accomodation_id) REFERENCES accomodation(accomodation_id)
+	);
+
+CREATE TABLE accomodation_calender(
+	accomodation_calender_id int PRIMARY KEY,
+	valid_from date,
+	valid_to date,
+	is_available bool,
+	price_per_night numeric (12,4),
+	currency_id int,
+    FOREIGN KEY (currency_id) REFERENCES currency(currency_id)
+	);
+
+COMMIT;
---
+++ scripts/INSERT.sql
@@ -0,0 +1,56 @@
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('1','cilginselin123','cilginselin123@gmail.com','#%§$&FGDS','selin','schmidt');
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('2','gdemirkol315','gdemirkol315@gmail.com','#%§$&FGDS','görkem','demirkol');
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('3','darkdefender','darkdefender@gmail.com','#%§$&FGDS','osman','abi');
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('4','airbnb_user_test','airbnb_user_test@gmail.com','#%§$&FGDS','airbnb_user_test_name','airbnb_user_test_last_name');
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('5','airbnb_user_test1','airbnb_user_test1@gmail.com','#%§$&FGDS','airbnb_user_test_name1','airbnb_user_test_last_name1');
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('6','airbnb_user_test2','airbnb_user_test2@gmail.com','#%§$&FGDS','airbnb_user_test_name2','airbnb_user_test_last_name2');
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('7','airbnb_user_test3','airbnb_user_test3@gmail.com','#%§$&FGDS','airbnb_user_test_name3','airbnb_user_test_last_name3');
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('8','airbnb_user_test4','airbnb_user_test4@gmail.com','#%§$&FGDS','airbnb_user_test_name4','airbnb_user_test_last_name4');
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('9','airbnb_user_test5','airbnb_user_test5@gmail.com','#%§$&FGDS','airbnb_user_test_name5','airbnb_user_test_last_name5');
+insert into account(account_id,user_name,email,password_hash,first_name,last_name) values('10','airbnb_user_test6','airbnb_user_test6@gmail.com','#%§$&FGDS','airbnb_user_test_name6','airbnb_user_test_last_name6');
+
+insert into social_media_type(social_media_type_id,social_media_platform_name) values (1,'Facebook');
+insert into social_media_type(social_media_type_id,social_media_platform_name) values (2,'Instagram');
+insert into social_media_type(social_media_type_id,social_media_platform_name) values (3,'Twitter');
+insert into social_media_type(social_media_type_id,social_media_platform_name) values (4,'Tiktok');
+
+insert into address(address_id, address, postal_code, city, country) values (1,'landstrasser hauptstrasse 21,3','1030','Wien','Österreich');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(1,1,1,'dafgagafjahfajklfah afasjashashkas','testemail@email.com','02125731363');
+insert into address(address_id, address, postal_code, city, country) values (2,'landstrasser teststrasse 21,2','1030','Wien','Österreich');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(2,2,2,'dafgagafjahfajklfah afasjashashkas','testemail@email.com','02125731363');
+insert into address(address_id, address, postal_code, city, country) values (3,'osmanstraße','1200','Wien','Österreich');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(3,3,3,'dafgagafjahfajklfah afasjashaskas','testemail@email.com','02125731363');
+insert into address(address_id, address, postal_code, city, country) values (4,'teststrasse 5','8950','Magic City','Atlantis');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(4,4,4,'dafgagafjahfajklfah ','testemail@email.com','02125731363');
+insert into address(address_id, address, postal_code, city, country) values (5,'landstrasser hauptstrasse 21,3','1030','Wien','Österreich');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(5,5,5,'dafgagafjahfajklfah afasjashashkas','testemail@email.com','02125731363');
+insert into address(address_id, address, postal_code, city, country) values (6,'landstrasser hauptstrasse 21,3','1030','Wien','Österreich');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(6,6,6,'dafgagafjahfajklfah afasjashashkas','testemail@email.com','02125731363');
+insert into address(address_id, address, postal_code, city, country) values (7,'landstrasser hauptstrasse 21,3','1030','Wien','Österreich');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(7,7,7,'dafgagafjahfajklfah afasjashashkas','testemail@email.com','02125731363');
+insert into address(address_id, address, postal_code, city, country) values (8,'landstrasser hauptstrasse 21,3','1030','Wien','Österreich');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(8,8,8,'dafgagafjahfajklfah afasjashashkas','testemail@email.com','02125731363');
+insert into address(address_id, address, postal_code, city, country) values (9,'landstrasser hauptstrasse 21,3','1030','Wien','Österreich');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(9,9,9,'dafgagafjahfajklfah afasjashashkas','testemail@email.com','02125731363');
+insert into address(address_id, address, postal_code, city, country) values (10,'landstrasser hauptstrasse 21,3','1030','Wien','Österreich');
+insert into guest(guest_id,invoice_address_id,account_id,profile_description,contact_email,contact_phone) values(10,10,10,'dafgagafjahfajklfah afasjashashkas','testemail@email.com','02125731363');
+
+insert into credit_card_vendor(credit_card_vendor_id, credit_card_vendor) values (1,'visa');
+insert into credit_card_vendor(credit_card_vendor_id, credit_card_vendor) values (2,'master');
+insert into credit_card_vendor(credit_card_vendor_id, credit_card_vendor) values (3,'maestro');
+insert into credit_card_vendor(credit_card_vendor_id, credit_card_vendor) values (4,'paypal');
+insert into credit_card_vendor(credit_card_vendor_id, credit_card_vendor) values (5,'paydirect');
+insert into credit_card_vendor(credit_card_vendor_id, credit_card_vendor) values (6,'americanexpress');
+
+insert into payment_status(payment_status_id, payment_status) values (1, 'PENDING');
+insert into payment_status(payment_status_id, payment_status) values (2, 'REJECTED');
+insert into payment_status(payment_status_id, payment_status) values (3, 'SUCCESSFUL');
+
+insert into accomodation_type(accomodation_type_id, accomodation_type) values (1, 'villa');
+insert into accomodation_type(accomodation_type_id, accomodation_type) values (2, 'apartment');
+insert into accomodation_type(accomodation_type_id, accomodation_type) values (3, 'private room');
+insert into accomodation_type(accomodation_type_id, accomodation_type) values (4, 'shared room');
+insert into accomodation_type(accomodation_type_id, accomodation_type) values (5, 'camping place');
+
+commit;
+-- insert into currency()values();
