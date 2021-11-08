59d9b8f027d0a279d0a64274f83476d3d8312629
goerkem.demirkol <goerkem.demirkol@bearingpoint.com>
2021-11-08 22:22:12 +0100

    NOT NULLs added to definitions

--- scripts/CREATE.sql
+++ scripts/CREATE.sql
@@ -3,50 +3,50 @@ USE AIRBNB;
 
 CREATE TABLE account(
 	account_id int PRIMARY KEY,
-	user_name varchar(50),
-	email varchar(50),
-	password_hash varchar(50),
-	first_name varchar(255),
-	last_name varchar(255)
+	user_name varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	password_hash varchar(50) NOT NULL,
+	first_name varchar(255) NOT NULL,
+	last_name varchar(255) NOT NULL
 	);
 CREATE TABLE social_media_type(
 	social_media_type_id int PRIMARY KEY,
-	social_media_platform_name varchar(50)
+	social_media_platform_name varchar(50) NOT NULL
 	);
 
 CREATE TABLE credit_card_vendor(
 	credit_card_vendor_id int PRIMARY KEY,
-	credit_card_vendor varchar(30)
+	credit_card_vendor varchar(30) NOT NULL
 	);
 
 CREATE TABLE payment_status(
 	payment_status_id int PRIMARY KEY,
-	payment_status varchar(10)
+	payment_status varchar(10) NOT NULL
 	);
     
 
 CREATE TABLE accommodation_type(
 	accommodation_type_id int PRIMARY KEY,
-	accommodation_type varchar(30)
+	accommodation_type varchar(30) NOT NULL
 	);
 	
 CREATE TABLE address(
 	address_id int PRIMARY KEY,
 	address varchar(255),
 	postal_code varchar(10),
-	city varchar(30),
-	country varchar(30)
+	city varchar(30) NOT NULL,
+	country varchar(30) NOT NULL
 	);
 
 CREATE TABLE currency(
 	currency_id int PRIMARY KEY,
-	iso_code varchar(3),
+	iso_code varchar(3) NOT NULL,
 	currency_name varchar(50)
 	);
 CREATE TABLE guest(
 	guest_id int PRIMARY KEY,
-	invoice_address_id int,
-	account_id int,
+	invoice_address_id int NOT NULL,
+	account_id int NOT NULL,
 	profile_description varchar(5000),
 	contact_email varchar(50),
 	contact_phone varchar(20),
@@ -55,8 +55,8 @@ CREATE TABLE guest(
 	);
 CREATE TABLE host(
 	host_id int PRIMARY KEY,
-	invoice_address_id int,
-	account_id int,
+	invoice_address_id int NOT NULL,
+	account_id int NOT NULL,
 	profile_description varchar(5000),
 	contact_email varchar(50),
 	contact_phone varchar(20),
@@ -69,34 +69,34 @@ CREATE TABLE host(
 
 CREATE TABLE social_media_profile_host(
 	social_media_profile_id int PRIMARY KEY,
-	host_id int,
-	social_media_type_id int,
-	account_url varchar(1000),
+	host_id int NOT NULL,
+	social_media_type_id int NOT NULL,
+	account_url varchar(1000) NOT NULL,
     FOREIGN KEY (host_id) REFERENCES host(host_id),
     FOREIGN KEY (social_media_type_id) REFERENCES social_media_type(social_media_type_id)
 	);
 
 CREATE TABLE social_media_profile_guest(
 	social_media_profile_id int PRIMARY KEY,
-	guest_id int,
-	social_media_type_id int ,
-	account_url varchar(1000),
+	guest_id int NOT NULL,
+	social_media_type_id int NOT NULL,
+	account_url varchar(1000) NOT NULL,
     FOREIGN KEY (guest_id) REFERENCES guest(guest_id),
     FOREIGN KEY (social_media_type_id) REFERENCES social_media_type(social_media_type_id)
 	);
 
 CREATE TABLE profile_pic_guest(
 	picture_id int PRIMARY KEY,
-	guest_id int,
-	file_location varchar(1000),
+	guest_id int NOT NULL,
+	file_location varchar(1000) NOT NULL,
 	title varchar(100),
 	FOREIGN KEY (guest_id) REFERENCES guest(guest_id) ON DELETE CASCADE
 	);
 
 CREATE TABLE profile_pic_host(
 	picture_id int PRIMARY KEY,
-	host_id int,
-	file_location varchar(1000),
+	host_id int NOT NULL,
+	file_location varchar(1000) NOT NULL,
 	title varchar(100),
     FOREIGN KEY (host_id) REFERENCES host(host_id) ON DELETE CASCADE
 	);
@@ -104,9 +104,9 @@ CREATE TABLE profile_pic_host(
 CREATE TABLE accommodation(
 	accommodation_id  int PRIMARY KEY,
     accommodation_type_id int,
-    address_id int,
+    address_id int NOT NULL,
     capacity int,
-    host_id int,
+    host_id int NOT NULL,
     description varchar(5000),
     FOREIGN KEY (address_id) REFERENCES address(address_id),
     FOREIGN KEY (accommodation_type_id) REFERENCES  accommodation_type(accommodation_type_id),
@@ -115,16 +115,16 @@ CREATE TABLE accommodation(
 
 CREATE TABLE booking_status(
 	booking_status_id int PRIMARY KEY,
-	booking_status varchar(10)
+	booking_status varchar(10) NOT NULL
 	);
 
 CREATE TABLE booking(
 	booking_id int PRIMARY KEY,
-	guest_id int,
-	accommodation_id int,
-	booking_from date,
-	booking_until date,
-	booking_status_id int,
+	guest_id int NOT NULL,
+	accommodation_id int NOT NULL,
+	booking_from date NOT NULL,
+	booking_until date NOT NULL,
+	booking_status_id int NOT NULL,
     FOREIGN KEY (guest_id) REFERENCES guest(guest_id),
     FOREIGN KEY (accommodation_id) REFERENCES accommodation(accommodation_id),
     FOREIGN KEY (booking_status_id) REFERENCES booking_status(booking_status_id)
@@ -132,7 +132,7 @@ CREATE TABLE booking(
 
 CREATE TABLE review(
 	review_id int PRIMARY KEY,
-	booking_id int,
+	booking_id int NOT NULL,
 	guest_comment varchar(2000),
 	host_comment varchar(2000),
 	guest_rating int,
@@ -143,7 +143,7 @@ CREATE TABLE review(
 
 CREATE TABLE payment(
 	payment_id int PRIMARY KEY,
-	booking_id int,
+	booking_id int NOT NULL,
 	total_amount numeric(12,4),
 	payment_date datetime,
 	credit_card_number varchar(50),
@@ -161,20 +161,20 @@ CREATE TABLE payment(
 
 CREATE TABLE accommodation_pic(
 	picture_id int PRIMARY KEY,
-	accommodation_id int,
-	file_location varchar(1000),
+	accommodation_id int NOT NULL,
+	file_location varchar(1000) NOT NULL,
 	title varchar(100),
     FOREIGN KEY (accommodation_id) REFERENCES accommodation(accommodation_id) ON DELETE CASCADE
 	);
 
 CREATE TABLE accommodation_calendar(
 	accommodation_calendar_id int PRIMARY KEY,
-	accommodation_id int,
-	valid_from date,
-	valid_to date,
-	is_available bool,
-	price_per_night numeric (12,4),
-	currency_id int,
+	accommodation_id int NOT NULL,
+	valid_from date NOT NULL,
+	valid_to date NOT NULL,
+	is_available bool NOT NULL,
+	price_per_night numeric (12,4) NOT NULL,
+	currency_id int NOT NULL,
     FOREIGN KEY (currency_id) REFERENCES currency(currency_id),
     FOREIGN KEY (accommodation_id) REFERENCES accommodation(accommodation_id) ON DELETE CASCADE
 	);
