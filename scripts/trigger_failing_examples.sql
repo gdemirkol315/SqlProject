0c171db63830c6684e3ad17b19f8c852ff36fdfb
goerkem.demirkol <goerkem.demirkol@bearingpoint.com>
2021-11-08 17:46:36 +0100

    data adjusted according to triggers

---
+++ scripts/trigger_failing_examples.sql
@@ -0,0 +1,6 @@
+INSERT INTO booking (booking_id, guest_id, accommodation_id, booking_from, booking_until, booking_status_id)
+    VALUES (61, 1, 13, str_to_date('06.05.2022','%d.%m.%Y'), str_to_date('06.06.2022','%d.%m.%Y'), 6);
+INSERT INTO booking (booking_id, guest_id, accommodation_id, booking_from, booking_until, booking_status_id)
+    VALUES (113, 19, 16, str_to_date('04.09.2021','%d.%m.%Y'), str_to_date('21.10.2021','%d.%m.%Y'), 4);
+INSERT INTO booking (booking_id, guest_id, accommodation_id, booking_from, booking_until, booking_status_id)
+    VALUES (131, 13, 40, str_to_date('23.06.2021','%d.%m.%Y'), str_to_date('28.07.2021','%d.%m.%Y'), 2);
